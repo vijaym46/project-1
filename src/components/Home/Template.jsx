7d7318dcf9d4214973237c775c9c5eebe90c1316
vijaym46 <vijaym4600@gmail.com>
2024-10-26 00:34:54 +0530

    without error productSLide comp, Home page completed

--- src/components/Home/Template.jsx
+++ src/components/Home/Template.jsx
@@ -2,6 +2,14 @@ import React, { useState } from 'react'
 import product1 from '../../assets/Product/product1.jpg'
 import product2 from '../../assets/Product/product2.jpg'
 import { FaCartArrowDown, FaEye } from 'react-icons/fa'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import ourProduct1 from '../../assets/OurProducts/ourProduct1.png'
+import ourProduct2 from '../../assets/OurProducts/ourProduct2.png'
+import ourProduct3 from '../../assets/OurProducts/ourProduct3.png'
+import ourProduct4 from '../../assets/OurProducts/ourProduct4.png'
+import ourProduct5 from '../../assets/OurProducts/ourProduct5.png'
 
 const Template = () => {
   const products = [
@@ -22,25 +30,52 @@ const Template = () => {
     setIsZoomed(!isZoomed);
   };    
 
+  const ourProducts = [ ourProduct1, ourProduct2, ourProduct3, ourProduct4, ourProduct5 ]
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+  };
+
+
   return (
     <div className='w-full flex flex-col items-center justify-center'>
 
       {/* --- Template heading --- */}
-      <div className='flex flex-col w-5/6 items-center justify-center shadow-[5px_0px_20px_0px_rgb(0,0,0,0.1);] rounded-xl py-5 gap-5'>
+      <div className='flex flex-col w-5/6 px-5 items-center justify-center shadow-[5px_0px_20px_0px_rgb(0,0,0,0.1);] rounded-xl py-5 gap-5'>
         <div className='flex gap-3 items-center justify-center'>
           <p className='w-10 h-[2px] bg-black'></p>
-          <h1 className='text-4xl font-semibold whitespace-nowrap'>Templates</h1>
+          <h1 className='text-3xl md:text-4xl font-semibold whitespace-nowrap'>Templates</h1>
           <p className='w-10 h-[2px] bg-black'></p>
         </div>
-        <p className='text-neutral-400 text-[17px]'>Choose From Our Ready-made Templates Lists</p>
-        <div className='flex gap-4'>
-          <button className='px-4 py-2 bg-orange-400 border-2 border-spacing-2 rounded'>Design Templates</button>
-          <button className='px-4 py-2 border-2 rounded hover:bg-orange-400 hover:text-white duration-200'>Blank Templates</button>
+        <p className='text-neutral-400 text-center text-[17px]'>Choose From Our Ready-made Templates Lists</p>
+        <div className='flex gap-1 sm:gap-4'>
+          <button className='px-2 sm:px-4 py-1 sm:py-2 bg-orange-400 border-2 whitespace-nowrap rounded'>Design Templates</button>
+          <button className='px-2 sm:px-4 py-1 sm:py-2 border-2 rounded whitespace-nowrap  hover:bg-orange-400 hover:text-white duration-200'>Blank Templates</button>
         </div>
       </div>
 
       {/* --- Template start --- */}
-      <div className='my-10 w-11/12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 lg:gap-8'>
+      <div className='my-10 w-11/12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8'>
         {products.map((product) => (
           <div key={product.id} >
             <div>
@@ -69,8 +104,28 @@ const Template = () => {
           </div>
         ))}
       </div>  
+
+      <button className='px-6 py-2 bg-orange-300 hover:bg-orange-400 duration-200 border-2 border-spacing-2 rounded-md mb-10'>View All Products</button>
       {/* --- Template end --- */}
 
+      {/* --- Our Other Products start */}
+      <div className='flex items-center justify-center'>
+        <p className='w-10 h-[2px] bg-black'></p>
+        <h1 className='text-2xl sm:text-4xl font-semibold whitespace-nowrap px-6 py-1 bg-[#EDF1FF]'>Our Other Products</h1>
+        <p className='w-10 h-[2px] bg-black'></p>
+      </div>
+      {/* --- Our Other Products end */}
+      <div className='px-5 pb-16 w-full'>
+        <div className="ourProduct-slide w-full mt-10">
+          <Slider {...settings}>
+            {ourProducts.map((ourProduct) => (
+              <div className='relative flex items-center justify-center border p-3 cursor-move'>
+                <img src={ourProduct} alt="ourProduct list" className='w-36'/>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
     </div>
   )
 }
